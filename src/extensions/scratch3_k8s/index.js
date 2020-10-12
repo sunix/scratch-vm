ecf2dc4945a063cca02d6c4fe8eb5946d8313047
Sun Tan <sutan@redhat.com>
2020-10-12 17:05:30 +0000

    Adding kubernetes blocks
    Signed-off-by: Sun Tan <sutan@redhat.com>

--- src/extensions/scratch3_k8s/index.js
+++ src/extensions/scratch3_k8s/index.js
@@ -1,11 +1,33 @@
 const ArgumentType = require('../../extension-support/argument-type');
 const BlockType = require('../../extension-support/block-type');
 const Cast = require('../../util/cast');
+const nets = require('nets');
 const log = require('../../util/log');
 
+const urlParams = new URLSearchParams(window.location.search);
+const urlBase = urlParams.get('k8sServer');
+
 class Scratch3K8sBlocks {
+
     constructor(runtime) {
         this.runtime = runtime;
+        this.namespace = "che";
+
+        this.getPodsPromise = new Promise((resolve, reject) => {
+            nets({
+                url: urlBase.replace("http://", "https://") // quick dirty fix should use https
+                    + this.namespace,
+                timeout: 60000
+            }, (err, res, body) => {
+                if (err) {
+                    log.warn(`error fetching get pods result! ${res}`);
+                    reject();
+                }
+                log.log(`Get pods from ${this.namespace}: ${body}`);
+                resolve(body);
+            })
+        });
+
     }
 
     getInfo() {
@@ -14,13 +36,58 @@ class Scratch3K8sBlocks {
             name: 'Kubernetes',
             blocks: [
                 {
-                    opcode: 'writeLog',
-                    blockType: BlockType.COMMAND,
-                    text: 'log [TEXT]',
+                    opcode: 'getPods',
+                    blockType: BlockType.REPORTER,
+                    text: 'getPods',
+                },
+                {
+                    opcode: 'getPodListSize',
+                    blockType: BlockType.REPORTER,
+                    text: 'get number of Pods [PODS_JSON]',
                     arguments: {
-                        TEXT: {
+                        PODS_JSON: {
                             type: ArgumentType.STRING,
-                            defaultValue: "hello"
+                            defaultValue: "[]"
+                        }
+                    }
+                },
+
+                {
+                    opcode: 'getPod',
+                    blockType: BlockType.REPORTER,
+                    text: 'get Pod [POD_INDEX] from List [PODS_JSON]',
+                    arguments: {
+                        POD_INDEX: {
+                            type: ArgumentType.NUMBER,
+                            defaultValue: 0
+                        }, PODS_JSON: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "[]"
+                        }
+                    }
+                },
+
+                {
+                    opcode: 'getPodName',
+                    blockType: BlockType.REPORTER,
+                    text: 'get Pod Name [POD_JSON]',
+                    arguments: {
+                        POD_JSON: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "{}"
+                        }
+                    }
+                },
+
+
+                {
+                    opcode: 'setNamespace',
+                    blockType: BlockType.COMMAND,
+                    text: 'Set namespace [NAMESPACE]',
+                    arguments: {
+                        NAMESPACE: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "che"
                         }
                     }
                 }
@@ -30,10 +97,37 @@ class Scratch3K8sBlocks {
         };
     }
 
-    writeLog(args) {
-        const text = Cast.toString(args.TEXT);
-        log.log(text.toUpperCase());
+    getPods(args) {
+        return this.getPodsPromise; //.then(pods => { log.log(`bouhhh ${pods}`); return pods[0].metadata.name;});
+    }
 
+    getPodListSize(args) {
+        const podsListJson = Cast.toString(args.PODS_JSON);
+        const pods = JSON.parse(podsListJson);
+        return pods.length;
+
+    }
+
+    getPod(args) {
+        const index = Cast.toNumber(args.POD_INDEX);
+        const podsListJson = Cast.toString(args.PODS_JSON);
+        const pods = JSON.parse(podsListJson);
+        return JSON.stringify(pods[index]);
+    }
+
+    getPodName(args) {
+        const podJson = Cast.toString(args.POD_JSON);
+
+        const pod = JSON.parse(podJson);
+        return pod.metadata.name;
+    }
+
+
+
+    setNamespace(args) {
+        const namespace = Cast.toString(args.NAMESPACE);
+        log.log(`Set current namespace with ${namespace}`);
+        this.namespace = namespace;
     }
 }
 
